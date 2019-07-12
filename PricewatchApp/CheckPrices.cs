8281d878a1c2e0b19964f4aa66c7d0716bb9bdce
Jan Jacobs <jan.jacobs@ewbl.vlaanderen.be>
2019-07-12 14:23:31 +0200

    Made checkprices exception proof

--- PricewatchApp/CheckPrices.cs
+++ PricewatchApp/CheckPrices.cs
@@ -27,49 +27,54 @@ namespace PricewatchApp
             using (var db = new PricewatchModel(System.Environment.GetEnvironmentVariable("CONNECTION_STRING")))
             {
                 foreach (App app in db.Apps.ToList()) {
-                    // Download and parse AppStore page
-                    var page = await StorePageFactory.Create(app.URL);
+                    try {
+                        // Download and parse AppStore page
+                        var page = await StorePageFactory.Create(app.URL);
 
-                    // Lookup the latestprice in the database
-                    var latestPrice = app.Prices.OrderBy(p => p.Date).LastOrDefault();
-                    if (latestPrice == null || latestPrice.Price1.CompareTo(page.Price) != 0)
-                    {
-                        // Add the new price to the database
-                        Price newPrice = new Price
+                        // Lookup the latestprice in the database
+                        var latestPrice = app.Prices.OrderBy(p => p.Date).LastOrDefault();
+                        if (latestPrice == null || latestPrice.Price1.CompareTo(page.Price) != 0)
                         {
-                            App1 = app,
-                            Price1 = page.Price,
-                            Date = DateTime.Now
-                        };
-                        app.Prices.Add(newPrice);
-
-                        // Download the app image
-                        using (WebClient client = new WebClient())
-                        {
-                            byte[] imageData = client.DownloadData(page.ImageUrl);
-                            string imgBase64 = System.Convert.ToBase64String(imageData);
-
-                            attachements.Add(new Attachment
+                            // Add the new price to the database
+                            Price newPrice = new Price
                             {
-                                Content = imgBase64,
-                                ContentId = Guid.NewGuid().ToString(),
-                                Filename = "image" + imageCounter++ + ".jpg", 
-                                Type = "image/jpeg",
-                                Disposition = "inline"
-                            });
-                        }
+                                App1 = app,
+                                Price1 = page.Price,
+                                Date = DateTime.Now
+                            };
+                            app.Prices.Add(newPrice);
 
-                        // Add info to the mail contents
-                        if (latestPrice != null)
-                        {
-                            float.TryParse(page.Price.Substring(0, page.Price.Length - 2).Replace(',', '.'), out float newPriceFloat);
-                            float.TryParse(latestPrice.Price1.Substring(0, latestPrice.Price1.Length - 2).Replace(',', '.'), out float latestPriceFloat);
+                            // Download the app image
+                            using (WebClient client = new WebClient())
+                            {
+                                byte[] imageData = client.DownloadData(page.ImageUrl);
+                                string imgBase64 = System.Convert.ToBase64String(imageData);
 
-                            string color = newPriceFloat < latestPriceFloat ? "darkgreen" : "darkred";
-                            mailContents += $"<tr><td><p><a href='{app.URL}'><img src='cid:{attachements.Last().ContentId}' width='175' height='175'></td><td style='vertical-align: top;'></a> <a href='{app.URL}'>{app.Name}</a> kost nu <span style='color:{color}'>{page.Price}</span> (in plaats van {latestPrice.Price1})!</p></td></tr>";
-                        } else {
-                            mailContents += $"<tr><td><p><a href='{app.URL}'><img src='cid:{attachements.Last().ContentId}' width='175' height='175'></td><td style='vertical-align: top;'></a> <a href='{app.URL}'>{app.Name}</a> kost nu {page.Price}!</p></td></tr>";
+                                attachements.Add(new Attachment
+                                {
+                                    Content = imgBase64,
+                                    ContentId = Guid.NewGuid().ToString(),
+                                    Filename = "image" + imageCounter++ + ".jpg", 
+                                    Type = "image/jpeg",
+                                    Disposition = "inline"
+                                });
+                            }
+
+                            // Add info to the mail contents
+                            if (latestPrice != null)
+                            {
+                                float.TryParse(page.Price.Substring(0, page.Price.Length - 2).Replace(',', '.'), out float newPriceFloat);
+                                float.TryParse(latestPrice.Price1.Substring(0, latestPrice.Price1.Length - 2).Replace(',', '.'), out float latestPriceFloat);
+
+                                string color = newPriceFloat < latestPriceFloat ? "darkgreen" : "darkred";
+                                mailContents += $"<tr><td><p><a href='{app.URL}'><img src='cid:{attachements.Last().ContentId}' width='175' height='175'></td><td style='vertical-align: top;'></a> <a href='{app.URL}'>{app.Name}</a> kost nu <span style='color:{color}'>{page.Price}</span> (in plaats van {latestPrice.Price1})!</p></td></tr>";
+                            } else {
+                                mailContents += $"<tr><td><p><a href='{app.URL}'><img src='cid:{attachements.Last().ContentId}' width='175' height='175'></td><td style='vertical-align: top;'></a> <a href='{app.URL}'>{app.Name}</a> kost nu {page.Price}!</p></td></tr>";
+                            }
                         }
+                    } catch (Exception e) { 
+                        // Ignore it if something went wrong, check the next price
+                        log.Error(e.Message, e);
                     }
                 }
 
